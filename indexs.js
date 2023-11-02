42744929f80dc7a8da46f97541e5e69f3f6a950d
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2023-11-01 18:40:16 -0700

    Add files via upload

---
+++ indexs.js
@@ -0,0 +1,65 @@
+// JQuery addClass examples
+
+// $("h1").addClass("big-title margin-50");
+
+// $("button");
+
+// JQuery selecting attributes examples
+
+// console.log($("img").attr("src"));
+// $("a").attr("href", "https://yahoo.com");
+
+// JQuery adding eventListener
+// $("h1").click(function() {
+    // $("h1").css("color", "purple");
+// });
+
+// for loop for eventListener in JQuery
+// for (let i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }
+
+// Click/press eventListeners in JQuery
+
+// $("button").click(function() {
+//     $("h1").css("color", "purple");
+// });
+
+// $("input").keypress(function(event) {
+//     $("h1").text(event.key);
+// });
+
+// $("h1").on("mouseover", function() {
+//     $("h1").css("color", "purple");
+// });
+
+// $("button").on("click", function() {
+//     $("h1").hide();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").toggle();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").fadeOut();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").fadeToggle();
+// });
+
+// $("button").on("click", function() {
+//     $("h1").slideToggle();
+// });
+
+// $("button").on("click", function() {
+    // $("h1").animate({opacity: 0.5}); // can only animate numeric values
+// });
+
+// $("button").on("click", function() {
+//     $("h1").slideUp().slideDown().animate({opacity:0.5}); //can be chained together
+// });
+
